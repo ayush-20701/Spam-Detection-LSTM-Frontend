6b419310c7bfd154bbd013633b6b787eb9e4ab4a
Ayush Kumar <ayushkumar20701@gmail.com>
2025-03-06 16:36:57 +0530

    api updated

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       setloading(true)
       setShowResult(false)
       // Call your Flask API
-      const response = await fetch("https://spam-detection-lstm-backend-zmu5.vercel.app/predict", {
+      const response = await fetch("https://spam-detection-lstm-backend.onrender.com/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
